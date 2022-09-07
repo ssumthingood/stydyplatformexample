e3c9e3d806f49cf8e87c0185f34a05db0493121d
ssumthingood <ssumthingood@gmail.com>
2022-09-07 18:36:37 +0900

    0907 redux example

--- libs/store/modules/index.ts
+++ libs/store/modules/index.ts
@@ -1,7 +1,8 @@
-import { AnyAction, combineReducers, EmptyObject } from "@reduxjs/toolkit";
+import { AnyAction, CombinedState, combineReducers, EmptyObject } from "@reduxjs/toolkit";
 import { HYDRATE } from "next-redux-wrapper";
+import counter from "./counter";
 
-const reducer = (state: EmptyObject | undefined, action: AnyAction) => {
+const reducer = (state: CombinedState<{ counter: { value: number } }> | undefined, action: AnyAction) => {
     if (action.type === HYDRATE) {
         return {
             ...state,
@@ -9,6 +10,7 @@ const reducer = (state: EmptyObject | undefined, action: AnyAction) => {
         };
     }
     return combineReducers({
+        counter,
         // 여기에 modules 폴더 내의 관리하려는 상태의 모듈을 추가
     })(state, action);
 };
