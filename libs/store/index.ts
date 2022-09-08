e5a79897b7857e73d27ef29d1d36dfc143caf1b7
ssumthingood <ssumthingood@gmail.com>
2022-09-08 11:49:18 +0900

    0908 redux example : complete?

--- libs/store/index.ts
+++ libs/store/index.ts
@@ -3,13 +3,13 @@ import rootReducer from "./reducers";
 import { MakeStore, createWrapper, Context } from "next-redux-wrapper";
 import { AnyAction, Store, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
-const bindMiddleware = (middleware: Middleware[]): StoreEnhancer => {
-    if (process.env.NODE_ENV !== "production") {
-        const { composeWithDevTools } = require("redux-devtools-extension");
-        return composeWithDevTools(applyMiddleware(...middleware));
-    }
-    return applyMiddleware(...middleware);
-};
+// const bindMiddleware = (middleware: Middleware[]): StoreEnhancer => {
+//     if (process.env.NODE_ENV !== "production") {
+//         const { composeWithDevTools } = require("redux-devtools-extension");
+//         return composeWithDevTools(applyMiddleware(...middleware));
+//     }
+//     return applyMiddleware(...middleware);
+// };
 
 // const makeStore: MakeStore<Store<any, AnyAction>> = () => {
 //     const store = createStore(rootReducer, {}, bindMiddleware([]));
